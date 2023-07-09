46c1c774f7cb3be114bf756c2b53f99b471cb1d3
adityavpai <58020554+adityavpai@users.noreply.github.com>
2023-07-09 13:16:10 +0530

    added breakfast menu header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit  
 streamlit.title("First App")
 
-streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard Boiled free range Egg")
+streamlit.header("Breakfast Favorites")
+streamlit.text(" 🥣 Omega 3 & Blueberry Oatmeal")
+streamlit.text("🥗Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔Hard Boiled free range Egg")
+streamlit.text("🥑🍞 Avocado Toast")
